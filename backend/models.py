4b1e25abd18a51539b5d34fe3d6a69a33f5e5438
nicolaskeller <nicolas.keller@student.unisg.ch>
2024-10-11 09:46:36 +0200

    initialization of db and description of a travelplan entity

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from database import db  # Make sure you import the `db` from `database.py`
+
+class TravelPlan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    itinerary = db.Column(db.JSON, nullable=False)  # Stores the travel plan
