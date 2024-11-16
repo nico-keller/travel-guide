f682f945a3e4c6a29e4ed4ca86da85da5b5e556b
em-i-ly <gallacher.emily@gmail.com>
2024-11-16 12:43:03 +0100

    added styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import PlanDetails from './pages/PlanDetails';
-import CreatePlan from './pages/CreatePlan'; // Import the CreatePlan component
+import CreatePlan from './pages/CreatePlan';
 import ImageGenerator from "./pages/ImageGenerator";
 import FlightSearch from './pages/FlightSearch';
+import './styles/App.css';
 
 function App() {
     return (
         <Router>
-            <div>
-                <h1>Welcome to Travel Guide</h1>
-                <Navbar/>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/plans/:id" element={<PlanDetails/>}/>
-                    <Route path="/create" element={<CreatePlan/>}/>
-                    <Route path="/generate-image" element={<ImageGenerator />} />
-                    <Route path="/search-flights" element={<FlightSearch />}/>
-                </Routes>
+            <div className="app-container">
+                <header className="app-header">
+                    <h1 className="app-title">Travel Guide</h1>
+                    <Navbar />
+                </header>
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/plans/:id" element={<PlanDetails/>}/>
+                        <Route path="/create" element={<CreatePlan/>}/>
+                        <Route path="/generate-image" element={<ImageGenerator />} />
+                        <Route path="/search-flights" element={<FlightSearch />}/>
+                    </Routes>
+                </main>
             </div>
         </Router>
     );
